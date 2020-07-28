6a757052b127ea2ff8f50961079961340bcbb304
Devison Santos <devison_santos@yahoo.com.br>
2020-07-28 16:51:56 -0300

    Desafio de POO da Loja Funcionando

--- poo/dasafio_main.py
+++ poo/dasafio_main.py
@@ -7,10 +7,14 @@ def main():
     vendedor = Vendedor('Pedro', 28, 5600)
     compra1 = Compra(cliente, datetime.now(), 600)
     compra2 = Compra(cliente, datetime(2020, 6, 4), 1500)
-    cliente.registrar_compra(compra1)
-    cliente.registrar_compra(compra2)
+    cliente.registrar_compras(compra1)
+    cliente.registrar_compras(compra2)
     print(f'Total: {cliente}', '(adulto)' if cliente.is_adulto() else '')
     print(f'Vendedor: {vendedor}')
+    valor_total = cliente.total_compras()
+    qtde_compras = len(cliente.compras)
+    print(f'Total: {valor_total} em {qtde_compras} compras')
+    print(f'Última compra: {cliente.ultima_compra()}')
 
 
 if __name__ == "__main__":
